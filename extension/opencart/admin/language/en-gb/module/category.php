bec7b556bae417a43b881b1ee15f52f7034c1eb5
AmarRaghuvanshi <arupareliya@animize.net>
2023-08-14 11:25:18 +0530

    name changes and login error fix
    seller=>tutor
    product=>course
    category=>level
    download=>material
    seller login fix
    sellerproduct form page tabs changes

--- extension/opencart/admin/language/en-gb/module/category.php
+++ extension/opencart/admin/language/en-gb/module/category.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Category';
+$_['heading_title']    = 'Level';
 
 // Text
 $_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified category module!';
-$_['text_edit']        = 'Edit Category Module';
+$_['text_success']     = 'Success: You have modified level module!';
+$_['text_edit']        = 'Edit Level Module';
 
 // Entry
 $_['entry_status']     = 'Status';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify category module!';
+$_['error_permission'] = 'Warning: You do not have permission to modify level module!';
